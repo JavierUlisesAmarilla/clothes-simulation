b5bbcc7df0632d94151137515d8ceb3d7b28e930
JavierUlisesAmarilla And <amarillajavierulises@gmail.com>
2023-04-04 17:19:09 -0500

    npm i --legacy-peer-deps

--- src/AmmoModel.jsx
+++ src/AmmoModel.jsx
@@ -5,6 +5,7 @@ import {Sphere, useGLTF} from '@react-three/drei'
 import {SoftBodyType, useSoftBody} from 'use-ammojs'
 import {useThree} from 'react-three-fiber'
 import {MODEL_SCALE} from './utils/constants'
+import {customDebug} from './utils/custom.debug'
 
 
 export const AmmoModel = () => {
@@ -17,6 +18,7 @@ export const AmmoModel = () => {
 
   if (isFirstRender) {
     isFirstRender = false
+    customDebug().log('AmmoModel: gltf.scene: ', gltf.scene)
     gltf.scene.scale.set(MODEL_SCALE, MODEL_SCALE, MODEL_SCALE)
     three.scene.add(gltf.scene)
   }
